04a9bf9cf21c1b05530d5f2192345349ef729e2c
Alan Berndt <alan@eatabrick.org>
2015-08-21 10:32:41 -0700

    Add registration form

--- cpanfile
+++ cpanfile
@@ -1,4 +1,7 @@
+requires 'Crypt::Eksblowfish::Bcrypt';
+requires 'DBIx::Class::EncodedColumn';
 requires 'Dancer2';
 requires 'Dancer2::Plugin::DBIC';
+requires 'Data::FormValidator';
 requires 'Template';
 requires 'YAML';
--- lib/WeightTracker.pm
+++ lib/WeightTracker.pm
@@ -1,10 +1,61 @@
 package WeightTracker;
-use Dancer2;
 
-our $VERSION = '0.1';
+use Dancer2;
+use Dancer2::Plugin::DBIC 'resultset';
+use Data::FormValidator;
+use Data::FormValidator::Constraints ':closures';
+use Data::FormValidator::Constraints::DateTime ':all';
 
 get '/' => sub {
-    template 'index';
+  template 'index';
 };
 
-true;
+get '/register' => sub {
+  template 'register';
+};
+
+post '/register' => sub {
+  my $results = Data::FormValidator->check(
+    scalar params, {
+      required => [qw[email password name gender height birth_date]],
+      optional => [qw[weight_goal]],
+      constraint_methods => {
+        email => [
+          email(),
+          sub {
+            my ($dfv, $email) = @_;
+            return not defined resultset('User')->find({ email => $email });
+          },
+        ],
+        password => sub {
+          my ($dfv, $password) = @_;
+          return $password eq params->{pass2};
+        },
+        gender      => qr/^[mf]$/,
+        height      => qr/^\d+(?:\D\d+\D*)?$/,
+        birth_date  => to_datetime('%Y-%m-%d'),
+        weight_goal => qr/^\d+$/,
+      },
+      filters       => 'trim',
+      field_filters => {
+        height => sub {
+          my ($height) = @_;
+          my @parts = split /\D/, $height;
+          return @parts == 2 ? 12 * $parts[0] + $parts[1] : $parts[0];
+        },
+      },
+    });
+
+  if ($results->success) {
+    if (my $user = resultset('User')->create(scalar $results->valid)) {
+      session user => $user;
+      redirect '/';
+    }
+  } else {
+    delete params->{$_} for $results->invalid;
+    template 'register',
+      { errors => { map { $_ => 1 } $results->missing, $results->invalid }, };
+  }
+};
+
+1;
